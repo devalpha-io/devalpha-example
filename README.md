ff87d33cfc8a00c22b239bfd29bbaa18f3b6bd14
Filip Hallqvist <filiphallqvist@gmail.com>
2018-04-18 00:08:45 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# DevAlpha example
+
+A simple example of piping data from Vester to DevAlpha in order to see charts and various backtesting metrics.
+
+## Usage
+
+Clone this repository.
+
+`$ git clone https://github.com/fhqvst/devalpha-example.git`
+
+Install dependencies.
+
+`npm install`
+
+Work the magic.
+
+`node index.js`
+
+## Data
+
+More free data like the included `MSFT.csv` can be found in the [Quandl WIKI Prices database](https://www.quandl.com/databases/WIKIP). Go check it out!
+
+## License
+
+MIT license. See the LICENSE file for details.
+
+## Responsibilities
+
+The author of this software is not responsible for any indirect damages (foreseeable or unforeseeable), such as, if necessary, loss or alteration of or fraudulent access to data, accidental transmission of viruses or of any other harmful element, loss of profits or opportunities, the cost of replacement goods and services or the attitude and behavior of a third party.
---
+++ index.js
@@ -0,0 +1,81 @@
+const path = require('path')
+const fs = require('fs');
+const _ = require('highland')
+const { vester } = require('vester')
+
+/* Setup some constants */
+const FILENAME    = 'MSFT.csv'
+const START_DATE  = new Date('2017-01-01').getTime()
+const END_DATE    = new Date('2017-12-30').getTime()
+
+/* Create a new stream and point it to the data */
+const stream = fs.createReadStream(path.resolve(FILENAME))
+
+/* Create a pipeline for parsing the input */
+const pipeline = _.pipeline(
+  _.split(),
+  _.map((item) => {
+    const parsed = item.split(',')
+    return {
+      identifier: parsed[0],
+      timestamp: new Date(parsed[1]).getTime(),
+      open: parseFloat(parsed[2]),
+      high: parseFloat(parsed[3]),
+      low: parseFloat(parsed[4]),
+      close: parseFloat(parsed[5])
+    }
+  }),
+  _.filter((item) => item.timestamp >= START_DATE && item.timestamp <= END_DATE)
+)
+
+/* Denotes whether or not we've entered a position. */
+let hasPosition = false
+
+/* Initialize Vester! */
+vester({
+  startCapital: 100000,
+  feeds: {
+    msftStream: _(stream).through(pipeline)
+  },
+  dashboard: {
+    active: true
+  }
+}, (context, action) => {
+  if (action.type === 'msftStream') {
+
+    /* Hmmm... */
+    const magicNumber = Math.random()
+
+    /* Enter a long position */
+    if (!hasPosition && magicNumber > 0.5) {
+      context.order({
+        identifier: 'MSFT',
+        price: action.payload.close,
+        quantity: 1000
+      })
+      hasPosition = true
+    }
+
+    /* Exit the long position */
+    if (hasPosition && magicNumber <= 0.5) {
+      context.order({
+        identifier: 'MSFT',
+        price: action.payload.close,
+        quantity: -1000
+      })
+      hasPosition = false
+    }
+
+  }
+})
+.errors((err) => {
+  /* Extract errors from the stream */
+  console.log('Whoops!', err.message)
+})
+.each(({ state, action }) => {
+  /* Consume the stream */
+  console.log('Received action:', action.type)
+})
+
+/* Open devalpha.herokuapp.com in your browser to start the backtest */
+console.log('Waiting for DevAlpha...')
